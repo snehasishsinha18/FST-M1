95bc4d6352253ab815892745d4bc34125546ecd6
snehasishsinha18 <snesinha@in.ibm.com>
2025-02-12 10:46:11 -0500

    Add files via upload

--- JUnit/Activities/Activity1.java
+++ JUnit/Activities/Activity1.java
@@ -1,47 +1,48 @@
-package Activitiy;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import java.util.HashMap;
-import java.util.Map;
-import org.testng.annotations.Test;
-import io.restassured.response.Response;
+package com.ibm.junit.JUnit_Project;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Activity1 {
-	String baseURI= "https://petstore.swagger.io/v2/pet";
-  @Test(priority = 1)
-  public void addpet() {
-	  
-	  Map<String, Object> body= new HashMap<>();
-	  body.put("id", 772329);
-	  body.put("name", "Riley");
-	  body.put("status", "alive");
-	
-	  Response res=given().baseUri(baseURI).header("Content-Type", "application/json").body(body).when().post();
-	  res.then().body("id", equalTo(772329));
-  }
-  
-  @Test(priority = 2)
-  public void getpet() {
-	  Response response = given().baseUri(baseURI).header("Content-Type", "application/json").when().pathParam("petId", 772329)
-	  .get("/{petId}"); 
-	  
-	// Assertion
-	response.then().body("id", equalTo(772329));
-	response.then().body("name", equalTo("Riley"));
-	response.then().body("status", equalTo("alive"));
+    // Test fixtures
+    static ArrayList<String> list;
+
+    // Initialize test fixtures before each test method
+    @BeforeEach
+    void setUp() throws Exception {
+        list = new ArrayList<String>();
+        list.add("alpha"); // at index 0
+        list.add("beta"); // at index 1
+    }
+
+    // Test method to test the insert operation
+    @Test
+    public void insertTest() {
+        // Assertion for size
+        assertEquals(2, list.size(), "Wrong size");
+        // Add new element
+        list.add(2, "charlie");
+        // Assert with new elements
+        assertEquals(3, list.size(), "Wrong size");
+
+        // Assert individual elements
+        assertEquals("alpha", list.get(0), "Wrong element");
+        assertEquals("beta", list.get(1), "Wrong element");
+        assertEquals("charlie", list.get(2), "Wrong element");
+    }
+
+    // Test method to test the replace operation
+    @Test
+    public void replaceTest() {
+        // Replace new element
+        list.set(1, "charlie");
+
+        // Assert size of list
+        assertEquals(2, list.size(), "Wrong size");
+        // Assert individual elements
+        assertEquals("alpha", list.get(0), "Wrong element");
+        assertEquals("charlie", list.get(1), "Wrong element");
+    }
 }
 
-@Test(priority = 3)
-public void deletePet() {
-	Response response = given()
-		.baseUri("https://petstore.swagger.io/v2/pet") // Set base URI
-		.header("Content-Type", "application/json") // Set headers
-		.when().pathParam("petId", 772329) // Set path parameter
-		.delete("/{petId}"); // Send DELETE request
-
-	// Assertion
-	response.then().body("code", equalTo(200));
-	response.then().body("message", equalTo("772329"));
-}
-}
---
+++ JUnit/Activities/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package com.ibm.junit.JUnit_Project;
+
+public class NotEnoughFundsException extends RuntimeException {
+
+    public NotEnoughFundsException(Integer amount, Integer balance) {
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+    }
+
+}
