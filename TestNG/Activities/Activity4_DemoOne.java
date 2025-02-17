bf275dca902d42f53dd25fc86b16565e5156ac05
snehasishsinha18 <snesinha@in.ibm.com>
2025-02-17 11:33:23 -0500

    Add files via upload

---
+++ TestNG/Activities/Activity4_DemoOne.java
@@ -0,0 +1,16 @@
+package fst_testng;
+
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoOne {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoOne Class");
+    }
+}
---
+++ TestNG/Activities/Activity4_DemoTwo.java
@@ -0,0 +1,11 @@
+package fst_testng;
+
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
